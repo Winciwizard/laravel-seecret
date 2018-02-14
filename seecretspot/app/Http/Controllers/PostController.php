85e661edba78e1d8e6aaaa4fa46149e29493be84
Winciwizard <vincent.verges@gmail.com>
2018-02-14 14:13:10 +0100

    Post validation et Outputting Error

--- seecretspot/app/Http/Controllers/PostController.php
+++ seecretspot/app/Http/Controllers/PostController.php
@@ -9,10 +9,16 @@ class PostController extends Controller
 {
     public function postCreatePost(Request $request)
     {
+        $this->validate($request, [
+            'body' => 'required|max:1000'
+        ]);
         $post = new Post();
         $post->body = $request['body'];
-        $request->user()->posts()->save($post);
+        $message = 'There was an error';
+        if($request->user()->posts()->save($post)) {
+            $message = 'Post successfully created!';
+        };
 
-        return redirect()->route('dashboard');
+        return redirect()->route('dashboard')->with(['message' => $message]);
     }
 }
